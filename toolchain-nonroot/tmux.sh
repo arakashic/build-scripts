9ed7e531b89259a2baa77065026ec38c62e6c34d
Yanfei Guo <yanf.guo@gmail.com>
2018-08-14 13:26:13 -0500

    update zsh and tmux

--- toolchain-nonroot/tmux.sh
+++ toolchain-nonroot/tmux.sh
@@ -6,8 +6,8 @@ library_path libevent
 pushd "$BUILD_HOME"
 
 # tmux
-wget https://github.com/tmux/tmux/releases/download/2.6/tmux-2.6.tar.gz
-tar zxf tmux-2.6.tar.gz
+wget https://github.com/tmux/tmux/releases/download/2.7/tmux-2.7.tar.gz
+tar zxf tmux-2.7.tar.gz
 SRC_DIR=$(find . -maxdepth 1 -type d -name "tmux*")
 pushd "$SRC_DIR"
 ./configure --prefix="$LR/tmux"
