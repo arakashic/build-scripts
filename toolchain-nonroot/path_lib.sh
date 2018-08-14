36c9f23a3c675e56fc85311db4a970e1996e0178
Yanfei Guo <yanf.guo@gmail.com>
2018-08-14 13:25:52 -0500

    update path_lib

--- toolchain-nonroot/path_lib.sh
+++ toolchain-nonroot/path_lib.sh
@@ -1,45 +1,43 @@
+#   (C) 2017 by Argonne National Laboratory.
+#       See COPYRIGHT in top-level directory.
 
-backup_path() {
-    if [ -z "$LBAK" ] ; then
-        export LBAK=1
-        export LBAK_PATH="$PATH"
-        export LBAK_MANPATH="$MANPATH"
-        export LBAK_C_INCLUDE_PATH="$C_INCLUDE_PATH"
-        export LBAK_CPLUS_INCLUDE_PATH="$CPLUS_INCLUDE_PATH"
-        export LBAK_LD_LIBRARY_PATH="$LD_LIBRARY_PATH"
-        export LBAK_LIBRARY_PATH="$LIBRARY_PATH"
-        if [ "$CURRENT_OS" = "LINUX" ]; then
-            export LBAK_PKG_CONFIG_PATH="$PKG_CONFIG_PATH"
-        fi
+if test -z "$BASE_PATH" ; then
+    readonly BASE_PATH=${LR:-$HOME/.local}
+    export BASE_PATH
+fi
+
+if test -z "$ENV_BACKUP_FILE" ; then
+    readonly ENV_BACKUP_FILE="$HOME/.path_lib.env.bak"
+fi
+
+# Utilities
+
+init() {
+    :
+}
+
+backup_env() {
+    if [ -f "$ENV_BACKUP_FILE" ]; then
+        rm "$ENV_BACKUP_FILE"
     fi
+    touch "$ENV_BACKUP_FILE"
+    {
+        echo "export PATH=$PATH"
+        echo "export CPATH=$CPATH"
+        echo "export INCLUDE=$INCLUDE"
+        echo "export C_INCLUDE_PATH=$C_INCLUDE_PATH"
+        echo "export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH"
+        echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
+        echo "export LIBRARY_PATH=$LIBRARY_PATH"
+        echo "export LBAK_PKG_CONFIG_PATH=$PKG_CONFIG_PATH"
+    } >> "$ENV_BACKUP_FILE"
 }
 
-recover_path() {
-    if [ -n "$LBAK" ] ; then
-        export LBAK=1
-        export PATH="$LBAK_PATH"
-        export MANPATH="$LBAK_MANPATH"
-        export C_INCLUDE_PATH="$LBAK_C_INCLUDE_PATH"
-        export CPLUS_INCLUDE_PATH="$LBAK_CPLUS_INCLUDE_PATH"
-        export LD_LIBRARY_PATH="$LBAK_LD_LIBRARY_PATH"
-        export LIBRARY_PATH="$LBAK_LIBRARY_PATH"
-        if [ "$CURRENT_OS" = "LINUX" ]; then
-            export PKG_CONFIG_PATH="$LBAK_PKG_CONFIG_PATH"
-        fi
-    fi
+restore_env() {
+    . "$ENV_BACKUP_FILE"
 }
 
-clear_backup_path() {
-    unset LBAK_PATH
-    unset LBAK_MANPATH
-    unset LBAK_C_INCLUDE_PATH
-    unset LBAK_CPLUS_INCLUDE_PATH
-    unset LBAK_LD_LIBRARY_PATH
-    unset LBAK_PKG_CONFIG_PATH
-    unset LBAK
-}
-
-has_path() {
+path_exists() {
     if [ -d "$1" ] || [ -h "$1" ]; then
         return 0
     else
@@ -47,42 +45,64 @@ has_path() {
     fi
 }
 
-lib_path() {
-    local BASE_PATH="$LR"
-    if has_path "$1" ; then
-        BASE_PATH="$1"
-    else
-        BASE_PATH="$BASE_PATH/$1"
-    fi
+clean_path() {
+    local _tmp_p=${(P)1}
+    export $1="${_tmp_p%:}"
+}
 
-    export C_INCLUDE_PATH="$BASE_PATH/include:$C_INCLUDE_PATH"
-    export CPLUS_INCLUDE_PATH="$BASE_PATH/include:$CPLUS_INCLUDE_PATH"
-    export LD_LIBRARY_PATH="$BASE_PATH/lib:$LD_LIBRARY_PATH"
-    export LIBRARY_PATH="$BASE_PATH/lib:$LIBRARY_PATH"
-    if has_path "$BASE_PATH/lib64" ; then
-        export LD_LIBRARY_PATH="$BASE_PATH/lib64:$LD_LIBRARY_PATH"
-        export LIBRARY_PATH="$BASE_PATH/lib64:$LIBRARY_PATH"
-    fi
-    if [ "$CURRENT_OS" = "LINUX" ]; then
-        if has_path "$BASE_PATH/lib/pkgconfig" ; then
-            export PKG_CONFIG_PATH="$BASE_PATH/lib/pkgconfig:$PKG_CONFIG_PATH"
+check_export_path() {
+    local _tmp_p=${(P)1}
+    if path_exists "$2"; then
+        if [[ ! ":$_tmp_p:" = *":$2:"* ]]; then
+            export $1="$2:$_tmp_p"
+            clean_path "$1"
         fi
+    else
+        return 0
     fi
 }
 
-application_path() {
-    local BASE_PATH="$LR"
-    if has_path "$1" ; then
-        BASE_PATH="$1"
-    else
-        BASE_PATH="$BASE_PATH/$1"
+abs_or_local_path() {
+    if path_exists "$1" ; then
+        NEW_PATH="$1"
+        return 0
     fi
+    if path_exists "$BASE_PATH/$1" ; then
+        NEW_PATH="$BASE_PATH/$1"
+        return 0
+    fi
+    echo "path_lib: $1 does not exist"
+    return 1
+}
 
-    export PATH="$BASE_PATH/bin:$PATH"
-    # fi
-    if has_path "$BASE_PATH/share/man" ; then
-        export MANPATH="$BASE_PATH/share/man:$MANPATH"
-    fi
+# Basic path functions
+
+lib_path() {
+    abs_or_local_path "$1"
+
+    check_export_path CPATH "$NEW_PATH/include"
+    export INCLUDE="$CPATH"
+    export C_INCLUDE_PATH="$CPATH"
+    export CPLUS_INCLUDE_PATH="$CPATH"
+
+    check_export_path LIBRARY_PATH "$NEW_PATH/lib"
+    check_export_path LIBRARY_PATH "$NEW_PATH/lib64"
+    export LD_LIBRARY_PATH="$LIBRARY_PATH"
+
+    check_export_path PKG_CONFIG_PATH "$NEW_PATH/lib/pkgconfig"
+    check_export_path PKG_CONFIG_PATH "$NEW_PATH/lib64/pkgconfig"
+}
+
+bin_path() {
+    abs_or_local_path "$1"
+
+    check_export_path PATH    "$NEW_PATH/bin"
+}
+
+# Convenient functions
+
+application_path() {
+    bin_path "$1"
 }
 
 library_path() {
@@ -90,45 +110,21 @@ library_path() {
 }
 
 compiler_path() {
-    application_path "$1"
+    bin_path "$1"
     lib_path "$1"
 }
 
 default() {
-    if has_path "$LR/usr/bin" ; then export PATH="$LR/usr/bin:$PATH" ; fi
-    if has_path "$LR/bin" ; then export PATH="$LR/bin:$PATH" ; fi
-    if has_path "$LR/usr/share/man" ; then export MANPATH="$LR/usr/share/man:$MANPATH" ; fi
-    if has_path "$LR/share/man" ; then export MANPATH="$LR/share/man:$MANPATH" ; fi
-    if has_path "$LR/usr/include" ; then
-        export C_INCLUDE_PATH="$LR/usr/include:$C_INCLUDE_PATH"
-        export CPLUS_INCLUDE_PATH="$LR/usr/include:$CPLUS_INCLUDE_PATH"
-    fi
-    if has_path "$LR/include" ; then
-        export C_INCLUDE_PATH="$LR/include:$C_INCLUDE_PATH"
-        export CPLUS_INCLUDE_PATH="$LR/include:$CPLUS_INCLUDE_PATH"
-    fi
-    if has_path "$LR/usr/lib" ; then
-        export LIBRARY_PATH="$LR/usr/lib:$LIBRARY_PATH"
-        export LD_LIBRARY_PATH="$LR/usr/lib:$LD_LIBRARY_PATH"
-    fi
-    if has_path "$LR/usr/lib64" ; then
-        export LIBRARY_PATH="$LR/usr/lib64:$LIBRARY_PATH"
-        export LD_LIBRARY_PATH="$LR/usr/lib64:$LD_LIBRARY_PATH"
-    fi
-    if has_path "$LR/lib" ; then
-        export LIBRARY_PATH="$LR/lib:$LIBRARY_PATH"
-        export LD_LIBRARY_PATH="$LR/lib:$LD_LIBRARY_PATH"
-    fi
-    if has_path "$LR/lib64" ; then
-        export LIBRARY_PATH="$LR/lib64:$LIBRARY_PATH"
-        export LD_LIBRARY_PATH="$LR/lib64:$LD_LIBRARY_PATH"
-    fi
-    if [ "$CURRENT_OS" = "LINUX" ]; then
-        _pkg_list=$(find "$LR" -type d -name "pkgconfig" | xargs | tr ' ' ':')
-        if [ -n "${_pkg_list}" ] ; then
-            export PKG_CONFIG_PATH="${_pkg_list}:$PKG_CONFIG_PATH"
-        fi
-    fi
+    if [ -n "$LOCAL_DEF_PATH" ]; then return; fi
+
+    check_export_path PATH    "$LR/usr/bin"
+    check_export_path PATH    "$LR/bin"
+    bin_path "bin"
+    bin_path "usr/bin"
+
+    lib_path "usr"
+    lib_path "$BASE_PATH"
+    export LOCAL_DEF_PATH=1
 }
 
 # vim: syn=zsh
