27f4e9fe503978d336ecb0013447e52cb80fb20a
Yanfei Guo <yanf.guo@gmail.com>
2018-08-20 11:29:37 -0500

    update zsh

--- toolchain-nonroot/zsh.sh
+++ toolchain-nonroot/zsh.sh
@@ -4,7 +4,7 @@ source common.sh
 
 pushd "$BUILD_HOME"
 
-wget -O zsh-5.5.1.zip "https://github.com/zsh-users/zsh/archive/zsh-5.4.2.zip"
+wget -O zsh-5.5.1.zip "https://github.com/zsh-users/zsh/archive/zsh-5.5.1.zip"
 unzip zsh-5.5.1.zip
 SRC_DIR=$(find . -maxdepth 1 -type d -name "zsh*")
 pushd "$SRC_DIR"
